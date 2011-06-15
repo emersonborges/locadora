07fae78eecc467b88687736fb5ff3b5a75c33fbb
Emerson Borges <emersonbrg@gmail.com>
2011-06-15 14:06:12 -0300

    Junit configurado.

---
+++ test/locadora/padroes/ObserverTest.java
@@ -0,0 +1,23 @@
+package locadora.padroes;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+
+
+
+public class ObserverTest {
+
+	
+	@Test
+	public void teste1(){
+		assertEquals(10, 10);
+	}
+	
+	public void teste2(){
+		
+	}
+	
+	
+}
