c05db29cb7d9b6533519fe5ee770da81a6a2b1ee
Jefferson <jefferson.almeida.comp@gmail.com>
2011-06-17 20:14:46 -0300

    Diagramas do projeto para visualizacao

---
+++ HsqlDb /hsqldb-2.2.4/hsqldb/readme.txt
@@ -0,0 +1,9 @@
+Readme File
+$Date: 2011-06-06 18:22:05 -0400 (Mon, 06 Jun 2011) $
+This package contains HyperSQL v. 2.2.4
+
+HyperSQL is a relational database engine and a set of tools written in Java.
+HyperSQL is also known as HSQLDB.
+
+The file "index.html" explains the contents of this distribution and has
+links to documentation and support resources.
--- src/locadora/persistence/hibernate/MyHibernateSingleton.java
+++ src/locadora/persistence/hibernate/MyHibernateSingleton.java
@@ -5,6 +5,18 @@ import org.hibernate.cfg.Configuration;
 
 public class MyHibernateSingleton {
 	
+	
+	/*
+	 * Podemos Utilizar
+	 * 
+	 *  
+	 * HSQLDB (http://hsqldb.org),
+	 * H2 (http://h2database.com), 
+	 * Derby (http://b.apache.org/derby), 
+	 * and Java DB (http://developers.sun.com/javadb). 
+	 * 
+	 * 
+	 * */
 	private static SessionFactory factory;
 	
 	private MyHibernateSingleton(){
