3942c56b5b20911814122d9a2353fb91e37db442
Jefferson <jefferson.almeida.comp@gmail.com>
2011-06-17 13:47:31 -0300

    arquivos deletados

--- src/locadora/persistence/hibernate/TesteAutomovel.java
+++ src/locadora/persistence/hibernate/TesteAutomovel.java
@@ -3,6 +3,9 @@ package locadora.persistence.hibernate;
 import org.hibernate.*;
 import java.util.Date;
 import java.util.List;
+
+import locadora.model.automovel.Carro;
+import locadora.model.factory.FactoryChevrolet;
 public class TesteAutomovel {
 
     public static void main(String[] args) {
@@ -10,6 +13,7 @@ public class TesteAutomovel {
     	Session sessao = MyHibernateSingleton.getInstance().openSession(); //Abrindo uma sess�o
         Transaction transaction = sessao.beginTransaction(); //Iniciando uma transa��o
         
+        Carro astra = (Carro) FactoryChevrolet.getInstance().createAutomovel("Astra");
         
         
         transaction.commit(); //Finalizando a transa��o                
