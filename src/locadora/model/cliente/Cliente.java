5122f0fc4d3977752e9bf7ff5916786801c2270b
Emerson Borges <emersonbrg@gmail.com>
2011-06-17 15:36:07 -0300

    xmls Pessoa e Cliente adicionados.

--- src/locadora/model/cliente/Cliente.java
+++ src/locadora/model/cliente/Cliente.java
@@ -10,6 +10,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import locadora.model.Pessoa;
 import locadora.model.desconto.*;
 import locadora.model.observer.*;
 
@@ -17,65 +19,19 @@ import locadora.model.observer.*;
  *
  * @author Emerson
  */
-@Entity
-public abstract class Cliente extends Observer{
+
+public abstract class Cliente extends Pessoa{
+	    
 	
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
-
-    private String celular;
-    private String cep;
-    private String cpf;
-    private Date dataNascimento;
-    private String email;
-    private String endereco;
-    private String nome;
-    private String nomeMae;
-    private String nomePai;
-    private String rg;
-    private String telefone;
-    Desconto desconto;
-    
-    private String updateStatus;
-    
+	private String cidade;
+	protected Desconto desconto;
+	private String updateStatus;
+	
+	
     public double desconto(){
         return this.desconto.desconto();
     }
-
-    public String getCelular() {
-        return celular;
-    }
-
-    public void setCelular(String celular) {
-        this.celular = celular;
-    }
-
-    public String getCep() {
-        return cep;
-    }
-
-    public void setCep(String cep) {
-        this.cep = cep;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public Date getDataNascimento() {
-        return dataNascimento;
-    }
-
-    public void setDataNascimento(Date dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
-
+	
     public Desconto getDesconto() {
         return desconto;
     }
@@ -84,75 +40,19 @@ public abstract class Cliente extends Observer{
         this.desconto = desconto;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getNomeMae() {
-        return nomeMae;
-    }
-
-    public void setNomeMae(String nomeMae) {
-        this.nomeMae = nomeMae;
-    }
-
-    public String getNomePai() {
-        return nomePai;
-    }
-
-    public void setNomePai(String nomePai) {
-        this.nomePai = nomePai;
-    }
-
-    public String getRg() {
-        return rg;
-    }
-
-    public void setRg(String rg) {
-        this.rg = rg;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
 	public String getUpdateStatus() {
 		return updateStatus;
 	}
 
 	public void setUpdateStatus(String updateStatus) {
 		this.updateStatus = updateStatus;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getCidade() {
+		return cidade;
 	}	
 }
